e8b516151f1b373ff38f107cb91aff19a1399f25
Aline Soares da Silva <56769013+Aline595@users.noreply.github.com>
2020-02-18 20:03:13 -0300

    Update Character.java

--- src/atv_02/Character.java
+++ src/atv_02/Character.java
@@ -5,7 +5,7 @@ public abstract class Character {
     WeaponBeharvior weapon;
 
     public void fight() {
-
+        System.out.println("Lutando. ");
     }
 
     public void setWeapon(WeaponBeharvior w) {
